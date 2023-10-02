825229d9f5ebf0f9defe1ed85192ed2cd639ca57
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-10-02 13:44:11 -0400

    Combined Modal to work (I was being dumb)

--- Store/src/components/Categories/ProductsComponents/ProductsCards.jsx
+++ Store/src/components/Categories/ProductsComponents/ProductsCards.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react'
-import { Card, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
-import ProductsModal from './ProductsModal';
+import { 
+    Card, 
+    CardBody, 
+    CardFooter, 
+    Image,
+    Button, 
+    Modal, 
+    ModalContent, 
+    ModalHeader, 
+    ModalBody, 
+    ModalFooter } from "@nextui-org/react";
 
 export default function ProductsCards() {
     const list = [
@@ -78,34 +87,73 @@ export default function ProductsCards() {
 
     return (
         <div className="flex flex-wrap w-full h-auto gap-4 items-center justify-center sm:px-4 px-10">
-        {list.map((item, index) => (
-            <Card shadow="sm" key={index} isPressable onPress={() => handleOpenModal(item)}>
-            <CardBody className="overflow-visible p-2">
-                <Image
-                shadow="sm"
-                radius="lg"
-                width="100%"
-                alt={item.title}
-                className="lg:w-[250px] w-full md:max-w-[300px] object-cover h-full md:max-h-[300px] drop-shadow-md"
-                src={item.img}
-                />
-            </CardBody>
-            <CardFooter className="flex flex-col w-full text-small gap-y-4">
-                <div className="flex flex-row w-full text-small justify-between font-semibold tracking-wide px-10">
-                <h1>{item.title}</h1>
-                <p className="text-zinc-500">${item.price}.99</p>
-                </div>
-                <div className="flex w-full">
-                {selectedProduct && (
-                    <ProductsModal
-                        selectedProduct={selectedProduct}
-                        onCloseModal={handleCloseModal}
-                    />
-                )}
-                </div>
-            </CardFooter>
-            </Card>
-        ))}
+            {list.map((item, index) => (
+                <Card shadow="sm" key={index}>
+                    <CardBody className="overflow-visible p-2">
+                        <Image
+                            shadow="sm"
+                            radius="lg"
+                            width="100%"
+                            alt={item.title}
+                            className="lg:w-[250px] w-full md:max-w-[300px] object-cover h-full md:max-h-[300px] drop-shadow-md"
+                            src={item.img}
+                        />
+                    </CardBody>
+                    <CardFooter className="flex flex-col w-full text-small gap-y-4">
+                        <div className="flex flex-row w-full text-small justify-between font-semibold tracking-wide px-10">
+                            <h1>{item.title}</h1>
+                            <p className="text-zinc-500">${item.price}.99</p>
+                        </div>
+                        <div className="flex w-full">
+                            <Button
+                                onClick={() => handleOpenModal(item)}
+                                className="flex flex-row items-center justify-center bg-green-500 w-full h-auto py-4 cursor-pointer"
+                                radius="lg"
+                            >
+                                <span>Add to cart</span>
+                                <ion-icon name="cart"></ion-icon>
+                            </Button>
+                        </div>
+                    </CardFooter>
+                    {selectedProduct && (
+                        <Modal
+                            isOpen={true}
+                            onOpenChange={handleCloseModal}
+                            className="bg-[#131313]"
+                        >
+                            <ModalContent>
+                                <ModalHeader className="flex flex-col gap-1 bg-slate-700">
+                                    {selectedProduct.title}
+                                </ModalHeader>
+                                <ModalBody className="flex w-full h-fit">
+                                    <img src={selectedProduct.img} alt={selectedProduct.title} />
+                                </ModalBody>
+                                <ModalFooter>
+                                    <div className="flex flex-row items-center justify-between w-full h-auto px-4">
+                                        <span>${selectedProduct.price}.99</span>
+                                        <div className="flex gap-x-2">
+                                            <Button color="danger" variant="ghost" onPress={handleCloseModal}>
+                                                Close
+                                            </Button>
+                                            <Button
+                                                color="success"
+                                                variant="flat"
+                                                onPress={() => {
+                                                    // Handle adding the selected product to the cart here
+                                                    handleCloseModal();
+                                                }}
+                                                className="text-white"
+                                            >
+                                                Add to cart
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    )}
+                </Card>
+            ))}
         </div>
     );
 }
