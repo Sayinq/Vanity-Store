98a27d1593254ed9eee3d42bbdca5f9d2f4e1f0a
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-10-05 14:53:33 -0400

    Pages fix attempt #3

--- Store/vite.config.js
+++ Store/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Vanity-Store/",
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
-})
+});
