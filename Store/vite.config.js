23a90d7c2fbefb91e78ed3da6e191998c5f8ebff
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-10-05 15:07:09 -0400

    deploy fix attempt #5

--- Store/vite.config.js
+++ Store/vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     outDir: 'build',
+    chunkSizeWarningLimit: 1000 * 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
   },
   plugins: [react()],
 });
